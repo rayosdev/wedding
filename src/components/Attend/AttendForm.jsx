8328ece5178d083e9d4bd4a79ca477f61470faac
jais01 <jais01@lillestrom.kommune.no>
2024-05-14 07:42:04 +0200

    vipps paypal and other fixes

--- src/components/Attend/AttendForm.jsx
+++ src/components/Attend/AttendForm.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import './AttendForm.scss'
-import PersonFormStep from './PersonFormStep'
-import branchWingsDecor from '../../assets/decor-branch-wings.svg' 
+import PersonFormStep from './PersonFormStep' 
+
+import decorationRightSVG from '../../assets/decoration-green--right.svg'
+import decorationLeftSVG from '../../assets/decoration-green--left.svg'
 
 export default function AttendForm() {
   return (
     <>
-        <form className='attend-form'>
+      <div className="attend-form--wrapper">
+        <form className="attend-form">
           <div className="attend-form__content">
             <h2>Will you attend?</h2>
-          <p>We have limited space so let us know by signing up until June 2nd
-            so we can plan accordingly</p>
+          <p>We have limited space, so please let us know by signing up before June 2nd so we can plan accordingly</p>
             <PersonFormStep />
-            <img src={branchWingsDecor} alt="" />
           </div>
         </form>
+        <img src={decorationLeftSVG} alt="" className="attend-form__branch--left"/>
+        <img src={decorationRightSVG} alt="" className="attend-form__branch--right"/>
+      </div>
     </>
   )
 }
--- src/components/EndSection.jsx
+++ src/components/EndSection.jsx
@@ -9,7 +9,7 @@ export default function EndSection() {
     return (
         <section className="end">
             <div className="container">
-                <h2>“We’re looking forward to party with you”</h2>
+                <h2>“We’re looking forward to celebrate with you”</h2>
             </div>
             <div className="end__image-wrapper">
                 <img src={branchSrc} alt="" className="no-pointer-event" />
